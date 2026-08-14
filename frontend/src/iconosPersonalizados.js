f40a42e467fcbf5cccad7cf863eb66bff46be594
RicardoMeVi <rmedina@icsh.com.mx>
2026-08-14 10:59:32 -0700

    Separar check-icon.html en componente React + CSS, usarlo como icono de Alarmas

--- frontend/src/iconosPersonalizados.js
+++ frontend/src/iconosPersonalizados.js
@@ -2,6 +2,7 @@ import { EarthLoader } from "./components/EarthLoader";
 import { FtpIcon } from "./components/FtpIcon";
 import { SmsIcon } from "./components/SmsIcon";
 import { CalendarIcon } from "./components/CalendarIcon";
+import { CheckIcon } from "./components/CheckIcon";
 
 // Secciones con un ícono propio (en vez del genérico de icons.svg).
 // Cada componente recibe una prop `size` (px).
@@ -10,4 +11,5 @@ export const ICONOS_PERSONALIZADOS = {
   ftp: FtpIcon,
   mensajes: SmsIcon,
   "fecha-hora": CalendarIcon,
+  alarmas: CheckIcon,
 };
